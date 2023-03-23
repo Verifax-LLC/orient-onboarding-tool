245b0f10a840ae6bf39b73a1e8afa7c884f11a15
Sean Lawton <sean@verifax.app>
2023-03-22 19:13:58 -0500

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,64 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      lg: "744px",
+      xl: "1184px",
+    },
+    colors: {
+      primaryLight: "#BD7DFF",
+      primaryDark: "#6819B0",
+    },
+    fontFamily: {
+      sans: [
+        '"Segoe UI"',
+        'Roboto',
+        'sans-serif',
+      ],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: [
+      {
+        verifax: {
+          primary: "#C9A2FF",
+          secondary: "#C1C8E4",
+          accent: "#C9A2FF",
+          neutral: "#71E1E1",
+          exotic: "#F59F01",
+          "base-100": "#ffffff",
+
+          "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.25s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-text-case": "uppercase", // set default text transform for buttons
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "1px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.5rem", // border radius of tabs
+        },
+      },
+      "dark",
+    ],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+  },
+};
